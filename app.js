1e2b7ed186914dc7456a01ccff67c1c1b48fddf6
Nickolas Fuentes <nickfuentes24@gmail.com>
2019-10-03 00:46:48 -0500

    Created some routes to query the data for oild wells, field, and lease name and status

--- app.js
+++ app.js
@@ -6,10 +6,34 @@ const models = require("./models")
 app.use(cors())
 app.use(express.json())
 
-app.get("/oil-wells", (req, res) => {
+// Search  By API14
+app.get("/oil-well", (req, res) => {
   models.Eagleford.findAll({
     where: {
       api14: 42051340170000
+    }
+  }).then(wellsinfo => {
+    res.json(wellsinfo)
+  })
+})
+
+// Search By Field
+app.get("/field", (req, res) => {
+  models.Eagleford.findAll({
+    where: {
+      field: "BRISCOE RANCH"
+    }
+  }).then(wellsinfo => {
+    res.json(wellsinfo)
+  })
+})
+
+// Search By Lease Name and Well Status
+app.get("/lease-name-well-status", (req, res) => {
+  models.Eagleford.findAll({
+    where: {
+      leaseName: "R.H. PICKENS, ET AL B19 UNIT",
+      wellStatus: "ACTIVE"
     }
   }).then(wellsinfo => {
     res.json(wellsinfo)
