c6036c9d982637959a0f8a43591472ce9ab2cb62
Taliaa <taliaa10@ymail.com>
2019-10-05 22:24:31 -0500

    heroku

---
+++ config/dev.js
@@ -0,0 +1,16 @@
+module.exports = {
+  type: "service_account",
+  project_id: "nesh-capstone",
+  sessionId: "981dbc33-7c54-5419-2cce-edf90efd2170",
+  languageCode: "en-US",
+  private_key_id: "76a8106918e02011a3e8abee19c941ebecc913fc",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvAIBADANBgkqhkiG9w0BAQEFAASCBKYwggSiAgEAAoIBAQDc9DWpMwhQ38C7\neilkMiWnBHx3MP8ZYn5YOF4apiVgd7DSjkN/yBfQo+/rgTMrMvc7Yl/3dXE+VKV9\nmkiGOj7dsdnoeL3rsVv/4RDofaHsWnRSlwnUC5LhfxotfEFgb/B7uLrb781cPsNw\n88opDR+KtJZOkEfNncCNi3Wq63VqQR+u+rDqoCF9KfwGoG+x7BV1iUyjiWOmvkAt\nB6VH/S2fxufEXJqUBNun2aDrFqMSe1A+GNtCI1wppyYyqbpjNLR/gV4QOgGBabcM\n1QfH9L9U5V0L1zMGPy6kvM7wMEdaF+GO6eyJYRR+UWdYfHVDbc1ZTOJ2KsIOh36d\n3SUlvrS5AgMBAAECggEABlE+BqujgN9PKr9ae8CvAw4ckTJsN6BfFIw3dkpPGNda\nFtMReI/Ag7REOuSctfzXROkJEhqylWZJdCkbLwUBc0iGR6ml0Z1mfcYNhTTC7e6N\nk0vMFTZVgGZM2c6Bv7sMv/KKrb2SRXyzVkFX3yZ1jDI0CRtv80Ro5VVsXcVRzk+n\nKwRhXNrMtr0X7rrZi5vgoH90rXVne8o2LlFB/ySO2/A6ujLONSH+PSJY0dWYg9EY\n1jAi5PBrPkQ2ZWof7hQsibSvsAfllBwE/rbz0fg1v6PXSBRfWo/PBbEE0gEOA8TN\n41xTDCUJzcePCAcYiafTbRHO1bsuz8qeun4iDBuhxwKBgQD7dhaMB7Svk7Iagyzp\nan+61tVMVeGxjUSwkj6wW26af2vhLkEZ+klJvUKPyozK99poqiRstINWF2S/RvzB\nhqvaoCg/I+HJ5WvaTf6hifXX04hIXvw3ooxZ7aSIYSojbeMR0mn3lsS7B3p5iWb0\nANl0OzVcHBUOxTTgJzsGIHMa5wKBgQDg8Sh7LgUe2bb8jziBXcL/oJhDM4AVUaiG\nGH7EaFkswhMNW+bs8La/SWTYXYC7CPgx2fyoLYSEr0XulpYycHPYGmCNTbIUdSh7\n2T3xtuRjr48OV8mK4+9aSFP1TfrT0W8CrHntwn+WfaGBobVLcg3/qf/r+8E2odzt\nJGVoJhRfXwKBgEX5ZWrQFiKpqe0gxZWqkiKu96oHWAetdQjhmVpABzYs8Rs3YNxW\nvqArb2NODQHKGNf5SKuLS+TgG6PyoBFGZXowCSMQ3BrvoaSdAZd4KZZGge0JqYvd\nysqXi6p7x1sWkBJ8iApixRn6icVaS/Dob5qdg0q/ccirbfxWTmF1FDLPAoGAWklR\np0eMdJqMcQkHdB1vRG1wUCDeuy/ghZpVX35GtMldHlKR+O6RW2wZFyNIpUnvXQsS\n92n1s9AqMJ6uwKYUSVwk/vJ9lOqFilhjHZ78QurXb7lawu3ER6eaU26uS9DXTupt\nsjoMPhpE9iQzAYpRBhp1B4IwRj5nz6N5NHl8Wd8CgYAZ0GXS0NZ0+QzFGBmt0HEn\nY30yx7pGa0TcX+aQjdtKBcV8SSZdPpksHXjyS2tbSEWLXLzHz65l1uSUr+V5ewQ1\noHtvyJ5QdQZaeNMNhE+boc/qKG/UWJT3ux2b/Qp8UPLnsfuKCPQB+1eEa4nbgXUB\nebe0bYo1pmptnYZP450Blw==\n-----END PRIVATE KEY-----\n",
+  client_email: "nesh-capstone@nesh-capstone.iam.gserviceaccount.com",
+  client_id: "118445873928577417164",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/nesh-capstone%40nesh-capstone.iam.gserviceaccount.com"
+};
