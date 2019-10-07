e395d9fefb1a4427ce690a35b89f57a2dbfb6917
Nickolas Fuentes <nickfuentes24@gmail.com>
2019-10-07 09:51:20 -0500

    Add chatbot window.

---
+++ client/src/Components/pages/Landing.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: "center" }}>
+      <h1>Selling You Great Stuff!</h1>
+    </div>
+  )
+}
+
+export default Landing
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import Map from './Components/Map'
+import React from "react"
+import ReactDOM from "react-dom"
+import "./index.css"
+import * as serviceWorker from "./serviceWorker"
+import App from "./components/App"
+import "materialize-css/dist/css/materialize.css"
 
-ReactDOM.render(<Map />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById("root"))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.unregister()
