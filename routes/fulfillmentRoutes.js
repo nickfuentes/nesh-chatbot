1e944db0c6a5fd2d5713ce2478061da8c44abaa4
Taliaa <taliaa10@ymail.com>
2019-10-09 13:50:45 -0500

    fixing tostring error

--- routes/fulfillmentRoutes.js
+++ routes/fulfillmentRoutes.js
@@ -1,10 +1,11 @@
-const { WebhookClient } = require("dialogflow-fulfillment")
-const models = require("../models")
-const utils = require("../utils/helpers")
+const { WebhookClient } = require("dialogflow-fulfillment");
+const models = require("../models");
+const utils = require("../utils/helpers");
+const { Payload } = require("dialogflow-fulfillment");
 
 module.exports = app => {
   app.post("/", async (req, res) => {
-    const agent = new WebhookClient({ request: req, response: res })
+    const agent = new WebhookClient({ request: req, response: res });
 
     mapWells = async agent => {
       try {
@@ -12,40 +13,65 @@ module.exports = app => {
           where: {
             diBasin: agent.parameters.Basin
           }
-        })
+        });
 
-        console.log(agent.parameters.Basin)
-        console.log(wells)
+        console.log(agent.parameters.Basin);
+        console.log(wells);
 
         const wellLocations = wells.map(well => {
           let wellLocation = {
             long: well.dataValues.surfaceHoleLongitude,
             lat: well.dataValues.surfaceHoleLatitude
-          }
-          return wellLocation
-        })
+          };
+          return wellLocation;
+        });
 
-        console.log(wellLocations)
+        let locations = [];
+
+        let location = {
+          location: wellLocations
+        };
+
+        locations.push(location);
+        console.log(locations);
+
+        console.log(wellLocations);
         const responseText = `Here are the well locations for ${utils.titleCase(
           agent.parameters.Basin
-        )}`
-        const wellsText = `${wellLocations}`
-        agent.add(wellsText)
-        agent.add(responseText)
-        console.log(utils.titleCase(agent.parameters.Basin))
+        )}`;
+        const wellsText = `${wellLocations}`;
+        // agent.add(locations);
+        agent.add(wellsText);
+        // agent.add([location]);
+        // agent.add(JSON.stringify(location));
+        agent.add(responseText);
+        // agent.add(`hey`);
+
+        agent.add(
+          new Payload("custom_payload", {
+            results: [
+              {
+                Title: "Some text",
+                Link: "example.com"
+              }
+            ]
+          })
+        );
+
+        console.log(utils.titleCase(agent.parameters.Basin));
       } catch (error) {
-        console.log(error)
+        console.log(error);
       }
-    }
+    };
 
     function fallback(agent) {
-      agent.add(`Webhook I didn't understand`)
-      agent.add(`Webhook I'm sorry, can you try again?`)
+      agent.add(`Webhook I didn't understand`);
+      agent.add(`Webhook I'm sorry, can you try again?`);
     }
 
-    let intentMap = new Map()
-    intentMap.set("Map Wells", mapWells)
-    intentMap.set("Default Fallback Intent", fallback)
-    agent.handleRequest(intentMap)
-  })
-}
+    let intentMap = new Map();
+    intentMap.set("Map Wells", mapWells);
+    intentMap.set("Default Fallback Intent", fallback);
+    agent.handleRequest(intentMap);
+  });
+};
--- utils/helpers.js
+++ utils/helpers.js
@@ -1,5 +1,8 @@
 exports.titleCase = str => {
-  str = str.toLowerCase().split(" ");
+  str = str
+    .toString()
+    .toLowerCase()
+    .split(" ");
   for (var i = 0; i < str.length; i++) {
     str[i] = str[i].charAt(0).toUpperCase() + str[i].slice(1);
   }
