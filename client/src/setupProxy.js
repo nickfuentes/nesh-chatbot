7800789d2bd623d92318ef99032bf2274d9b9346
Taliaa <taliaa10@ymail.com>
2019-10-12 14:00:14 -0500

    proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -3,7 +3,8 @@ const proxy = require("http-proxy-middleware");
 module.exports = function(app) {
   app.use(
     proxy("/api", {
-      target: "http://127.0.0.1:5000",
+      target: "http://localhost:5000",
+      changeOrigin: true,
       headers: {
         Connection: "keep-alive"
       }
