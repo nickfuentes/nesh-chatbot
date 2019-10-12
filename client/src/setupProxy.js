e3886f1fb96918658486773f9cbac0bd16ee3e48
Taliaa <taliaa10@ymail.com>
2019-10-12 12:50:22 -0500

    proxy

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,5 +1,12 @@
 const proxy = require("http-proxy-middleware");
 
 module.exports = function(app) {
-  app.use(proxy("/api", { target: "https://nesh-chatbot.herokuapp.com/" }));
+  app.use(
+    proxy("/api", {
+      target: "https://nesh-chatbot.herokuapp.com/",
+      headers: {
+        Connection: "keep-alive"
+      }
+    })
+  );
 };
