cb3e7785cbd905af8d18f6ede327f0d6277001ef
Taliaa <taliaa10@ymail.com>
2019-10-13 18:53:36 -0500

    adding center well map locations

--- client/src/Components/map/Map.js
+++ client/src/Components/map/Map.js
@@ -15,6 +15,34 @@ const WellsMap = props => {
     };
   };
 
+  const getMapBounds = (map, maps, places) => {
+    const bounds = new maps.LatLngBounds();
+
+    props.wellsInfo.forEach(place => {
+      bounds.extend(new maps.LatLng(place.lat, place.long));
+    });
+    return bounds;
+  };
+
+  // Re-center map when resizing the window
+  const bindResizeListener = (map, maps, bounds) => {
+    maps.event.addDomListenerOnce(map, "idle", () => {
+      maps.event.addDomListener(window, "resize", () => {
+        map.fitBounds(bounds);
+      });
+    });
+  };
+
+  // Fit map to its bounds after the api is loaded
+  const apiIsLoaded = (map, maps, places) => {
+    // Get bounds by our places
+    const bounds = getMapBounds(map, maps, places);
+    // Fit map to bounds
+    map.fitBounds(bounds);
+    // Bind the resize listener
+    bindResizeListener(map, maps, bounds);
+  };
+
   const [center] = useState({ lat: 29.7954, lng: -95.5698 });
   const [zoom] = useState(5);
   return (
@@ -24,6 +52,10 @@ const WellsMap = props => {
         defaultCenter={center}
         defaultZoom={zoom}
         options={getMapOptions}
+        yesIWantToUseGoogleMapApiInternals
+        onGoogleApiLoaded={({ map, maps }) =>
+          apiIsLoaded(map, maps, props.wellsInfo)
+        }
       >
         {props.wellsInfo.map((well, i) => {
           return (
